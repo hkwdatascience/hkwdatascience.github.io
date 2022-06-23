dc1fa8b94d1e27bc805a5a0502939394268483d5
hkwdatascience <106625702+hkwdatascience@users.noreply.github.com>
2022-06-23 16:17:03 -0400

    Final changes before formally using git commands

--- nav.js
+++ nav.js
@@ -1,3 +1,6 @@
+//A short js script to help share the navigation bar across the website
+//directly copied from page: https://stackoverflow.com/questions/31954089/how-can-i-reuse-a-navigation-bar-on-multiple-pages
+
 fetch('nav.html')
 .then(res => res.text())
 .then(text => {
