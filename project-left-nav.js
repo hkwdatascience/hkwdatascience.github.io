03be7446d5861437a4c5d85793dd868895955bcd
Bruce Wang <hkwdatascience@gmail.com>
2022-07-15 23:26:24 -0400

    Added the following files to repo. Forgot to do this last commit.
    css/project-styling.css
    jupyter-notebooks/F1-jupyter.html
    machine-learning-algo.html
    project-left-nav.html
    project-left-nav.js

---
+++ project-left-nav.js
@@ -0,0 +1,11 @@
+//A short js script to help share the navigation bar across the website
+//directly copied from page: https://stackoverflow.com/questions/31954089/how-can-i-reuse-a-navigation-bar-on-multiple-pages
+
+fetch('project-left-nav.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#replace_with_project-left-navbar");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+})
