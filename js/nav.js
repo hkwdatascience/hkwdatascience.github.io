aea4fe3666612a3505bf5936f35b4e8fed55e019
Bruce Wang <hkwdatascience@gmail.com>
2022-07-20 09:25:56 -0400

    restructure of JS files
    1.moved all js files into js/ folder
    2.changed font and style in many of the pages
    3.changed some styling in the CSS files.

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,7 @@
 //A short js script to help share the navigation bar across the website
 //directly copied from page: https://stackoverflow.com/questions/31954089/how-can-i-reuse-a-navigation-bar-on-multiple-pages
 
-fetch('nav.html')
+fetch('../nav.html')
 .then(res => res.text())
 .then(text => {
     let oldelem = document.querySelector("script#replace_with_navbar");
